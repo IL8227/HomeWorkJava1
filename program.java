e753c9b9552831f205eeef2dbe34937eb64c76bd
IliaV <iliaspb2012@gmail.com>
2022-11-24 00:54:38 +0300

    first task

---
+++ program.java
@@ -0,0 +1,42 @@
+// Вычислить n-ое треугольного число(сумма чисел от 1 до n),
+//  n! (произведение чисел от 1 до n)
+
+
+import java.util.Scanner;
+public class program { 
+    static int sum(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i ++) {
+            result = result + i;
+        }
+        return result;
+    }
+    static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i ++) {
+            result = result * i;
+        }
+        return result;
+           
+    } 
+
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Выберите операцию:\n1. Сложение.\n2. Факториал.\n");
+        int a = iScanner.nextInt();
+        System.out.printf("Введите число: ");
+        int n = iScanner.nextInt();
+        switch (a) {
+            case 1 : 
+            System.out.println(sum(n));
+            break;
+            case 2 :
+            System.out.println(factorial(n));
+            break;
+            
+        }
+    }
+}           
+    
+    
